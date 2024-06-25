5b03e641fc4e532c91401848afc02a4340985515
Miracle Otugo <67126036+MirakoolDev@users.noreply.github.com>
2024-06-25 19:51:09 +0100

    commit

---
+++ network.js
@@ -0,0 +1,9 @@
+//You can see a list of compatible EVM chains at https://chainlist.org/
+window.networks = [
+  {
+    id: 84532,
+    label: "Base Sepolia Testnet ETH",
+    token: "ETH",
+    rpcUrl: 'https://sepolia.base.org',
+  }
+]
